394eb54d9bde56dae44cb76153a4c9e9189ac045
lelia69 <elbouranialcaraz.lelia.simplon@gmail.com>
2022-06-17 14:49:14 +0200

    navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import Navbar from './Components/Navbar';
+import HomePage from './Pages/HomePage';
+import './index.css'
 
 
 
@@ -10,15 +12,10 @@ function App() {
 
   return (
     <BrowserRouter>
-
-
-
+      <Navbar />
       <Routes>
-
+        <Route path="/" element={<HomePage />} />
       </Routes>
-
-
-
     </BrowserRouter>
   );
 }
