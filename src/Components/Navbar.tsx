64790eedc98a58d30709525b8c30e2e75158d095
lelia69 <elbouranialcaraz.lelia.simplon@gmail.com>
2022-07-13 14:46:47 +0200

    add profile dropDown

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -5,6 +5,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import Banners from './Banners'
 import ModalLogin from './ModalLogin'
+import { useAppSelector } from '../App/hooks'
 
 const navigation = [
   { name: 'Mulule', href: '#', current: true },
@@ -16,6 +17,8 @@ function classNames(...classes: string[]) {
 }
 
 export default function Navbar() {
+
+  let user = useAppSelector(state => state.auth.user);
   return (
     <>
       <Disclosure as="nav" className="bg-gray-600/20 border-b border-red-400">
@@ -120,9 +123,9 @@ export default function Navbar() {
 
 
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                      <ModalLogin/>
-                  {/* Profile dropdown */}
-                  {/* <Menu as="div" className="ml-3 relative">
+                 
+                 
+                 {user ?<Menu as="div" className="ml-3 relative">
                     <div>
                       <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                         <span className="sr-only">Open user menu</span>
@@ -149,33 +152,25 @@ export default function Navbar() {
                               href="#"
                               className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                             >
-                              Your Profile
+                              Mon Compte
                             </a>
                           )}
                         </Menu.Item>
+                        
                         <Menu.Item>
                           {({ active }) => (
                             <a
                               href="#"
                               className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                             >
-                              Settings
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                            >
-                              Sign out
+                              Déconnexion
                             </a>
                           )}
                         </Menu.Item>
                       </Menu.Items>
                     </Transition>
-                  </Menu> */}
+                  </Menu> : <ModalLogin/> }
+                  
                 </div>
               </div>
             </div>
