6da8adbccc90b709013f20a3b4686d96d11089b9
Roland Foucher <roland.foucher.pro@gmail.com>
2022-08-11 13:36:14 +0200

    creatAPI

--- src/App/API/userConsiderations.ts
+++ src/App/API/userConsiderations.ts
@@ -1,4 +1,5 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 import { Considerations } from "../entities/considerations";
 import { prepare } from "../utils/token";
 
